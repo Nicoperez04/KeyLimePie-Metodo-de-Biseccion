1e89f03b6b6cf6e5b98594856dde3743d23d70aa
nicolasperez <nicoapgelp@gmail.com>
2025-09-16 22:39:40 -0300

    Deploy 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',                 // genera /out
-  basePath: 'KeyLimePie-Metodo-de-Biseccion',         // ej: '/KeyLimePie_Errores'
-  assetPrefix: 'KeyLimePie-Metodo-de-Biseccion',     // mismo subpath para assets
+  basePath: '/KeyLimePie-Metodo-de-Biseccion',         // ej: '/KeyLimePie_Errores'
+  assetPrefix: '/KeyLimePie-Metodo-de-Biseccion',     // mismo subpath para assets
   images: { unoptimized: true },    // requerido con export
   trailingSlash: true               // opcional, ayuda en Pages
 };
